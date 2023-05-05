044ea8622c65589d4de4908f6bad810099588f34
manjurulcis <haque.mdmanzurul@gmail.com>
2023-05-05 18:46:37 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vitejs-output-file-paths-g2ttrf
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vitejs-output-file-paths-g2ttrf)
---
+++ vite.config.js
@@ -0,0 +1,22 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        assetFileNames: (assetInfo) => {
+          let extType = assetInfo.name.split('.').at(1);
+          if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
+            extType = 'img';
+          }
+          return `assets/${extType}/[name]-test[extname]`;
+        },
+        chunkFileNames: 'assets/js/[name]-test.js',
+        entryFileNames: 'assets/js/[name]-test.js',
+      },
+    },
+  },
+});
